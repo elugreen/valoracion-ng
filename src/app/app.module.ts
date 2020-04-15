f551e808614bcf5f81514a6c332f7e187fd67968
elugreen <58693204+elugreen@users.noreply.github.com>
2020-04-15 16:19:46 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatStepperModule} from '@angular/material/stepper';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,7 +23,14 @@ import {MatRadioModule} from '@angular/material/radio';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatRadioModule 
+    MatRadioModule,
+    MatStepperModule,
+    NgbModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,84 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {SelectionModel} from '@angular/cdk/collections';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatTableDataSource} from '@angular/material/table';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+
+export interface PeriodicElement {
+  Pieza: string;
+  position: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, Pieza: 'TABLERO PUERTA TRASERO DERECHO'},
+  {position: 2, Pieza: 'GUARDAFANGO DERECHO'},
+  {position: 3, Pieza: 'COSTADO DERECHO'},
+  {position: 4, Pieza: 'CINTA DECORATIVA ANTERIOR PUERTA TRASERO DERECHOd'},
+  {position: 5, Pieza: 'PUERTA DELANTERO DERECHO'},
+  {position: 6, Pieza: 'TABLERO PUERTA DELANTERO DERECHO'},
+  {position: 7, Pieza: 'COSTADO DERECHO'},
+  {position: 8, Pieza: 'GUARDAFANGO DERECHO'},
+  {position: 9, Pieza: 'ESTRIBO DERECHO'},
+  {position: 10, Pieza: 'PUERTA DELANTERO DERECHO'},
+];
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
+  }],
   styles: []
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+
+  displayedColumns: string[] = ['select', 'Pieza'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
 
   ngOnInit() {
+  }
+  currDiv: string = 'step-1';
+  currPdiv: string= 'select'
+
+  ShowDiv(divVal: string) {
+    this.currDiv = divVal;
+  }
+  ShowPdiv(divVal: string) {
+    this.currPdiv = divVal;
+  }
+  open_modal(content) {
+    this.modalService.open(content);
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
